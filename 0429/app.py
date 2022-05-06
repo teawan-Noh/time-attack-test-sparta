29fd66ab41183985aed7d4e470e46349779b023c
taewan <ntw2100@gmail.com>
2022-05-06 18:22:40 +0900

    timeattack1 update

--- 0429/app.py
+++ 0429/app.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client.test
+db = client.timeattack
 
 @app.route('/')
 def index():
@@ -15,22 +15,38 @@ def index():
 
 @app.route('/detail/<idx>')
 def detail(idx):
-      # // todo
-    return render_template('index.html')
+    post = db.memo.find_one({'idx': int(idx)})
+
+    return render_template('detail.html', post=post)
 
 
 @app.route('/post', methods=['POST'])
 def save_post():
     # // todo
     title_receive = request.form['title_give']
-    pw_receive = request.form['title_pw']
-    comment_receive = request.form['title_comment']
-    return {"result": "success"}
+    pw_receive = request.form['pw_give']
+    comment_receive = request.form['comment_give']
+    # print(title_receive, pw_receive, comment_receive)
+    # 날짜 가져오기
+    today = datetime.now()
+    #데이터 수 가져오기
+    count = db.memo.estimated_document_count()
+    next_index = count + 1
+
+    doc = {'idx' : next_index,
+            'title': title_receive,
+            'content': comment_receive,
+            'pw': pw_receive,
+            'reg_date': today.strftime('%Y.%m.%d %H:%M:%S')}
+
+    db.memo.insert_one(doc)
+
+    return {"result": "저장 완료"}
 
 
 @app.route('/post', methods=['GET'])
 def get_post():
-    posts = list(db.memotest.find({},{'_id':False}))
+    posts = list(db.memo.find({},{'_id':False}).sort('reg_date', -1))
 
     return jsonify({"posts": posts})
 
@@ -38,9 +54,10 @@ def get_post():
 @app.route('/post', methods=['DELETE'])
 def delete_post():
     idx = request.args.get('idx')
-    db.test.delete_one({'idx': int(idx)})
+    print(idx)
+    db.memo.delete_one({'idx': int(idx)})
     return {"result": "success"}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000,debug=True)
--- 0429/init_db.py
+++ 0429/init_db.py
@@ -1,21 +1,32 @@
-import requests
 from datetime import datetime
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client.test
+db = client.timeattack
 
-def insert_tour():
+def insert_memo():
     # 날짜 가져오기
     today = datetime.now()
     mytime = today.strftime('%Y.%m.%d %H:%M:%S')
 
     doc = {'idx' : 1,
-            'title': '제목1',
+            'title': '제목2',
             'content': '하하하',
             'pw': 1234,
             'reg_date': mytime}
 
-    db.memotest.insert_one(doc)
+    # db.memo.insert_one(doc)
+    print('a')
 
-insert_tour()
+    #인덱스 설정 어떻게 하는거냐
+    # db.memo.ensureIndex({'idx': 1})
+    # db.memo.create_index({'idx':1})
+    print(db.memo.estimated_document_count())
+
+insert_memo()
+
+
+
+
+
+
